3559522f6306a760e5e8888b7cbcf70d458a6726
Dmitry <dmitry.virtyal@gmail.com>
2022-12-09 19:34:03 +0700

    edit Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class Main {
     public static void main(String[] args) {
         TelegramBotsApi botsApi;
+        System.out.println("Start program");
         try {
             botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new MyBot());
